4b4c9063beab7116ef0b0f87bff4de5d6bd313a8
the-sociophobic <torikowashi@gmail.com>
2021-03-19 04:34:51 +0300

    Home first section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,44 @@
 import React from "react"
 
+import FormattedMessage from '../components/FormattedMessage'
+import ProgramPreview from '../components/ProgramPreview'
+import Link from '../components/Link'
+
 
 class Home extends React.Component<{}, {}> {
   render = () =>
     <div className="Home">
-      Home
+
+      <div className="Home__cover" />
+
+      <div className="container">        
+        <div className="row mb-s">
+          <FormattedMessage
+            id='Home.info'
+            className='col-6 col-md-10'
+          />
+        </div>
+        <div className="row">
+          <div className="col-4 col-md-3 col-xl-2">
+            <Link
+              to='/festival/about'
+              className="button--secondary mb-2"
+            >
+              <FormattedMessage id='Festival.About.name'/>
+            </Link>
+          </div>
+          <div className="col-4 col-md-3 col-xl-2">          
+            <Link
+              to='/festival/archive'
+              className="button--secondary mb-2"
+            >
+              <FormattedMessage id='Festival.Archive.name'/>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <ProgramPreview program='' />
     </div>
 }
 
