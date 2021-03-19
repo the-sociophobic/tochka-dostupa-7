42cca3fe7d4db7de349ac103eb24ebd5ba1a7a8f
the-sociophobic <torikowashi@gmail.com>
2021-03-19 03:42:27 +0300

    finished Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,7 +79,7 @@ const links = [
 .map(link =>
   <Link
     to={link.to}
-    className="Footer__links__item"
+    className="Footer__link Footer__link--regular"
   >
     {link.messageId ?
       <FormattedMessage id={link.messageId} />
@@ -94,39 +94,45 @@ const Footer : React.FunctionComponent = ({}) =>
     <div className="Footer__container">
       <div className="Footer__links">
         <div className="Footer__links__1">
-          <FormattedMessage id='AccessPoint'/>
+          <Link to='/' className="Footer__links__home" >
+            <div className="Footer__links__home__logo" />
+            <FormattedMessage
+              className="Footer__links__home__title"
+              id='AccessPoint'
+            />
+          </Link>
         </div>
         <div className="Footer__links__2">
           <FormattedMessage
             id='Program.name'
-            className='Footer__links__title'
+            className='Footer__link Footer__link--title'
           />
           {links.slice(0, 3)}
-          <div className="Footer__links__item" />
+          <div className="Footer__link Footer__link--regular" />
           {links[3]}
         </div>
         <div className="Footer__links__3">
           <FormattedMessage
             id='Festival.name'
-            className='Footer__links__title'
+            className='Footer__link Footer__link--title'
           />
           {links.slice(4, 10)}
         </div>
+        <div className="Footer__links__divider" />
         <div className="Footer__links__4">
           <FormattedMessage
             id='Footer.media'
-            className='Footer__links__title'
+            className='Footer__link Footer__link--title'
           />
           {links.slice(10, 14)}
         </div>
       </div>
       <div className="Footer__contacts">
         <div className="Footer__contacts__1">
-          <Link to='tel:+78126499206' className='p--xl'>
+          <Link to='tel:+78126499206' className='Footer__link Footer__link--email'>
             +7 (812) 649 92 06
           </Link>
-          <br />
-          <Link to='mailto:info@tochkadostupa.spb.ru' className='p--xl'>
+          <Link to='mailto:info@tochkadostupa.spb.ru' className='Footer__link Footer__link--email'>
             info@tochkadostupa.spb.ru
           </Link>
         </div>
@@ -136,16 +142,18 @@ const Footer : React.FunctionComponent = ({}) =>
             id="Footer.address"
           />
         </div>
+        <div className="Footer__contacts__divider" />
         <div className="Footer__contacts__3">
+          {links[14]}
+        </div>
+        <div className="Footer__contacts__divider" />
+        <div className="Footer__contacts__4">
           {links[15]}
         </div>
-        <div className="Footer__contacts__4">
-          {links[16]}
-        </div>
       </div>
-      <div className="Footer__disclaimer px-0">
+      <div className="Footer__disclaimer">
         <FormattedMessage
-          className="p--xs"
+          className="p--xs col-12 pb-xs"
           id="Footer.disclaimer"
         />
       </div>
