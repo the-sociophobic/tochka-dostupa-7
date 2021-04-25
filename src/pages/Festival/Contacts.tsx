b5a934318f813b7262cb06fc26221891bac67f9a
the_sociophobic <torikowashi@gmail.com>
2021-04-25 22:45:23 +0300

    Q&A

--- src/pages/Festival/Contacts.tsx
+++ src/pages/Festival/Contacts.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
 
+import Link from '../../components/Link'
+import { Context } from '../../components/Store'
+
+
 class Contacts extends React.Component<{}, {}> {
   
+  static contextType = Context
 
-  render = () =>
-    <div className="Contacts">
+  render = () => {
+    const page = this.context?.contentful?.festivalContactss[0]
 
-    </div>
+    return !page ? '' :
+      <div className="Contacts">
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <h1 className='h1 mt-s mt-md-m mt-lg-l mb-m mb-md-l mb-lg-xl'>
+                {page.name}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+  }
 }
 
 
--- src/pages/Festival/Projects.tsx
+++ src/pages/Festival/Projects.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
 
 
+import Link from '../../components/Link'
+import { Context } from '../../components/Store'
+
+
 class Projects extends React.Component<{}, {}> {
   
+  static contextType = Context
 
-  render = () =>
-    <div className="container opened-header">
-      <div className='row'>
-        <div className='col'>
+  render = () => {
+    const page = this.context?.contentful?.festivalProjectss[0]
 
-        </div>        
+    return !page ? '' :
+      <div className="Projects">
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <h1 className='h1 mt-s mt-md-m mt-lg-l mb-m mb-md-l mb-lg-xl'>
+                {page.name}
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='row'>
-        <div className='col'>
-
-        </div>        
-      </div>
-    </div>
+  }
 }
 
 
