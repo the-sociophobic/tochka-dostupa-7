63c9ac76f3263392f3b906c6978488e96c5a7d6f
the_sociophobic <torikowashi@gmail.com>
2021-06-15 05:26:53 +0300

    try react-snapshot

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import { render } from 'react-snapshot'
 import App from './App'
 
-ReactDOM.render(
+// ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
